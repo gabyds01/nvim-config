22441342c57e50f5fa4d5161c51d5d1c642d7a7c
gabyds01 <gabyds01@gmail.com>
2026-04-29 14:50:21 -0300

    Initial configuration

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- Global config
+vim.g.mapleader = ' '
+
+-- Options config
+vim.o.number = true
+
+-- for search
+vim.o.ignorecase = true
+
+-- for temp file if something wrong
+vim.o.swapfile = false
+vim.o.backup = false
+
+vim.o.scrolloff = 4
