29d4e0afaf88d31d20866c29a831e64cdf093a1e
gabyds01 <gabyds01@gmail.com>
2026-05-24 12:46:23 -0300

    refactor: move Neovim configuration options to a dedicated lua/options.lua file

--- init.lua
+++ init.lua
@@ -1,18 +1,4 @@
--- Global config
-vim.g.mapleader = ' '
-
--- Options config
-vim.o.number = true
-vim.o.relativenumber = true
-
--- for search
-vim.o.ignorecase = true
-
--- for temp file if something wrong
-vim.o.swapfile = false
-vim.o.backup = false
-
-vim.o.scrolloff = 4
-
+require "options"
 require "plugins"
 require "keymaps"
+
---
+++ lua/options.lua
@@ -0,0 +1,15 @@
+-- Global config
+vim.g.mapleader = ' '
+
+-- Options config
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- for search
+vim.o.ignorecase = true
+
+-- for temp file if something wrong
+vim.o.swapfile = false
+vim.o.backup = false
+
+vim.o.scrolloff = 4
